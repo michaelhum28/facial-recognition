bc9679fb8322f042dc8475756415866f1f0a1ccd
Michael Hum <michaelhum28@gmail.com>
2024-01-19 17:36:50 -0500

    testing camera pc

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-# app.py
-
-import cv2
-from flask import Flask, render_template, Response
-from camera import Camera
-import signal
-import sys
+from flask import Flask
+from flask import render_template
+import subprocess
 
 app = Flask(__name__)
 
@@ -12,24 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        if frame is not None:
-            ret, jpeg = cv2.imencode('.jpg', frame)
-            frame = jpeg.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def signal_handler(sig, frame):
-    print("Shutting down gracefully...")
-    sys.exit(0)
+@app.route('/run_python_script')
+def run_python_script():
+    result = subprocess.run(['python', 'FaceMeshModule.py'], capture_output=True, text=True)
+    return result.stdout
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, signal_handler)
     app.run(debug=True)
