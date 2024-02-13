1b499ceabf37ebfdd5558366dc3f04276cc9d696
Michael Hum <michaelhum28@gmail.com>
2024-02-12 23:17:49 -0500

    refix

--- stream.js
+++
@@ -1,9 +0,0 @@
-function stream(videoElement) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-            videoElement.srcObject = stream;
-        })
-        .catch(function (error) {
-            console.error('Error accessing webcam:', error);
-        });
-}
